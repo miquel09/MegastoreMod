756f37c2ba24cabba597c6e433ecf711f586df46
miquelOrdina <miquel.smits@ordina.nl>
2026-04-18 11:13:45 +0200

    added time manager

---
+++ MegastoreSimulator.GameLibs/Managers/TimeManager/TimeManagerPatches.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using InternalTimeManager = MegastoreSimulator.GameLibs.Managers.TimeManager.TimeManager;
+
+namespace MegastoreSimulator.GameLibs.Managers.TimeManager;
+
+internal class TimeManagerPatches
+{
+    [HarmonyPatch(typeof(global::TimeManager), nameof(global::TimeManager.OnEndDay))]
+    internal static class OnDayEndPatches
+    {
+        [HarmonyPostfix]
+        static void Postfix(global::TimeManager __instance)
+        {
+            Plugin.Logger.LogDebug($"### OnDayEnd");
+            var instance = new InternalTimeManager(__instance);
+            TimeManagerEvents.FireOnDayEnd(instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(global::TimeManager), nameof(global::TimeManager.StartTheNewDay))]
+    internal static class OnPaymentTakenPatches
+    {
+        [HarmonyPostfix]
+        static void Postfix(global::TimeManager __instance)
+        {
+            Plugin.Logger.LogDebug($"### OnNewDayStart");
+            var instance = new InternalTimeManager(__instance);
+            TimeManagerEvents.FireOnNewDayStart(instance);
+        }
+    }
+}
