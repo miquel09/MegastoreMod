545253b89b6223266952b8649b8a8880a7f4cf0d
miquelOrdina <miquel.smits@ordina.nl>
2026-05-28 17:13:32 +0200

    removed some things

---
+++ MegastoreSimulator.GameLibs/Managers/StatisticsManager/StatisticsManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using InternalProductType = MegastoreSimulator.GameLibs.Models.Enums.ProductType;
+
+namespace MegastoreSimulator.GameLibs.Managers.StatisticsManager;
+
+public class StatisticsManager
+{
+    #region Instance
+    private static global::StatisticsManager _instance;
+
+    internal static global::StatisticsManager Instance
+    {
+        private get
+        {
+            if (_instance == null)
+                throw new InvalidOperationException("StatisticsManager is not yet initialised");
+            return _instance;
+        }
+        set
+        {
+            _instance ??= value;
+        }
+    }
+    #endregion
+
+    public IReadOnlyDictionary<InternalProductType, int> SoldItems => GetCount(Instance.soldItemsDictionary);
+
+    private static IReadOnlyDictionary<InternalProductType, int> GetCount(Dictionary<global::ProductType, int> source)
+    {
+        var dictionary = new Dictionary<InternalProductType, int>();
+
+        foreach (var kvp in source)
+        {
+            dictionary[(InternalProductType)(int)kvp.Key] = kvp.Value;
+        }
+
+        return dictionary;
+    }
+}
